9d041a944d7ba6332c1a830567cdd5bcda91ea2a
madhu <madhutp11@gmail.com>
2025-06-30 19:17:41 +0530

    chnage of kill logic

--- start.sh
+++ start.sh
@@ -3,5 +3,15 @@
 cd ~/deploy_to_oci
 source venv/bin/activate
 pip install -r requirements.txt
-pkill -f uvicorn || true
+
+# Find process using port 5000 and kill it (if any)
+PID=$(sudo lsof -t -i:5002)
+
+if [ -n "$PID" ]; then
+    echo "Stopping process on port 5002 (PID: $PID)"
+    kill $PID
+fi
+
+# Start the app
 nohup venv/bin/uvicorn app:app --host 0.0.0.0 --port 5002 > app.log 2>&1 &
+echo "Started new app with PID: $!"
